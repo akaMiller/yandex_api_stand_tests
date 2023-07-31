6016978fde7ebf66ef95864f4683d4b0460440ec
Miller <faceofgun@yandex.com>
2023-07-31 10:48:27 +0200

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://28fb3877-d99b-44a9-9dd8-7c7f008ebdaf.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
